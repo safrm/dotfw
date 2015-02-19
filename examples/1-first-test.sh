d814d0b9aebd59fdc6006e72894450a5f922b8fe
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-02-19 16:57:40 +0200

    Int: minor fixes
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- examples/1-first-test.sh
+++ examples/1-first-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 APP=/usr/bin/xeyes
+DOTFW_PROCESS=1
 [ -z "$TCID" ] && . /usr/bin/dotfw
 
 teststart "lets try something easy- help"
@@ -13,7 +14,7 @@ testend
 
 teststart "now something on background"
 $APP &
-sleep 2
+sleep 10
 appProcessCount
 RET=$?
 if [ $RET -ne 1 ]; then
